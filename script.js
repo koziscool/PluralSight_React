24741ef4ce89168508d345bc4b468562e2d4f0fa
koziscool <kozjob@yahoo.com>
2016-09-23 03:09:11 -0500

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var Button = React.createClass({
+  
+  render: function() {
+    return (
+        <button> Go </button>
+    );
+  }
+  
+});
+
+
+ReactDOM.render( <Button />, document.getElementById("root") );
