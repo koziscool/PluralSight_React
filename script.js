3ef70d450cb3044342e56e0823f1a044e1e4ae77
koziscool <kozjob@yahoo.com>
2016-10-06 12:20:03 -0500

    child components, state, props

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 
 var Button = React.createClass({
   
-  getInitialState: function() {
-    return { counter: 0 }
-  },
-
-  handleClick: function() {
-    this.setState( { counter: this.state.counter+1 } );
-  },
-
   render: function() {
     return (
-        <button onClick={this.handleClick}> {this.state.counter } </button>
+        <button onClick={this.props.localHandleClick}> +1 </button>
     );
   }
   
 });
 
+var Result = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.localCounter}</div>
+    )
+  }
 
-ReactDOM.render( <Button />, document.getElementById("root") );
+});
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return { counter: 0 }
+  },
+ 
+  handleClick: function() {
+    this.setState( { counter: this.state.counter+1 } );
+  },
+ 
+  render: function() {
+    return (
+      <div>
+        <Button localHandleClick={this.handleClick} />
+        <Result localCounter={this.state.counter}/>
+      </div>
+    )
+  }
+});
+
+
+ReactDOM.render( <Main />, document.getElementById("root") );
