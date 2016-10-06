cd5a7c72c1a6a150aee5884083634d5969a6ab8e
koziscool <kozjob@yahoo.com>
2016-10-06 12:10:01 -0500

    counter increments

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ var Button = React.createClass({
     return { counter: 0 }
   },
 
+  handleClick: function() {
+    this.setState( { counter: this.state.counter+1 } );
+  },
+
   render: function() {
     return (
-        <button> {this.state.counter } </button>
+        <button onClick={this.handleClick}> {this.state.counter } </button>
     );
   }
   
